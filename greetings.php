907c016b4b1d3996cf2a9caac0c29bb42dd62111
Andrearusso94 <andrea.r.4845@gmail.com>
2022-11-25 15:27:28 +0100

    add cancel name

--- greetings.php
+++ greetings.php
@@ -5,6 +5,8 @@
 $name = $_GET['name'];
 $testo = $_GET['testo'];
 $greetings = "Il tuo testo: $testo ";
+//creo nuova variabile per nasconder il nome inserito
+$newName = str_ireplace($name, '***', $name);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,5 +20,9 @@ $greetings = "Il tuo testo: $testo ";
     <!--stampo a schermo il paragrafo con la sua lunghezza-->
     <h1> <?php echo $greetings; ?></h1>
     <h2> <?php echo 'Il tuo paragrafo è lungo '.strlen($testo) .' caratteri'; ?> </h2>
+    <!--anche se non richiesto stampo a schermo il nome inserito-->
+    <h3><?php echo $name; ?></h3>
+    <!--stampo nome censurato-->
+    <h3> <?php echo 'Ho censurato il nome ' . $newName; ?></h3>
 </body>
 </html>
